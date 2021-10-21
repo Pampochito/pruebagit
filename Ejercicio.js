7b16cc09579c3aa52f5516216adeb6aebd919ef2
Oso <ezequielrdrgz97@gmail.com>
2021-10-20 21:04:42 -0300

    Ejercicio Clase 4 SUMA

---
+++ Ejercicio.js
@@ -0,0 +1,8 @@
+// [10,20,55,321321,312,4043,4341,0,312321,1]
+let uno = [10,20,55,321321,312,4043,4341,0,312321,1]
+let suma = 0
+for (let contador =0; contador < uno.length; contador++){
+    suma = suma + uno[contador]
+
+}
+console.log (suma)
