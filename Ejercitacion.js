118dc23957952ce0ce1a53aeee6cfe7d4bef8988
Oso <ezequielrdrgz97@gmail.com>
2021-10-22 16:41:54 -0300

    Prueba

---
+++ Ejercitacion.js
@@ -0,0 +1,15 @@
+// Importamos nuestro array
+const movies = require('./movies')
+
+// Ejecucion
+
+/* Cantidad de películas por cada año */
+
+const moviesPerYear = () => {
+    let perYear = data.map((e) => {
+        console.log(e.Year)
+    })
+    return perYear
+}
+
+moviesPerYear() 
